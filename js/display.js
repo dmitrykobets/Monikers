2ed96c114b49be70b5e508189e5215e07025deb2
dmitrykobets <dmitry.kobets212@gmail.com>
2018-12-30 19:57:04 -0500

    Allow card picking/filtering at beginning of game.

--- js/display.js
+++ js/display.js
@@ -1,18 +1,17 @@
 var defaultHtml = '<h1>Welcome to Monikers!</h1> <div> <p class="numCards">Choose how many cards you would like to use: </p> <input class="numCards" type="number" id="numCardsInput" value="40"/> </div> <div> <p class="numCards">Team 1 Name: </p> <input class="numCards" type="text" id="team1Input"/> </div> <div> <p class="numCards">Team 2 Name: </p> <input class="numCards" type="text" id="team2Input"/> </div> <button type="button" onClick="startTurn()" id="startButton"><p>Start Round</p></button>'
-var aboutToStartTurnHtml = "<p>%roundname%</p><p>Choose someone from %nextteam%</p><p>Cards Gussed: %guessedCount%</p><p>Cards Passed: %passedCount%</p><p>Cards Left In Deck: %deckCount%</p>"
+var aboutToStartTurnHtml = "<p>%roundname%</p><p>Choose someone from %nextteam%</p><p>Cards Gussed: %guessedCount%</p><p>Cards Left In Deck: %deckCount%</p>"
 var roundOverHtml = "<p>Round %roundnum% is over!</p><p>%winningmsg%</p><p>%team1%: %1teampoints%</p><p>%team2%: %2teampoints%</p><br><p>%roundname%</p><p>Cards in Deck: %deckCount%</p><p>Choose someone from %nextteam%</p>"
 var aboutToStartGameHtml = "<p>%roundname%</p><p>Cards in Deck: %deckCount%</p><p>Choose someone from %nextteam%</p>"
 var gameOverHtml = "<p>Game Over!</p><p>%winningmsg%</p><p>%team1%: %1teampoints%</p><p>%team2%: %2teampoints%</p>"
 var button = '<button type="button" onClick="startTurn()" id="startButton"><p>Start Round</p></button>'
-var restartbutton = '<button type="button" onClick="restart()" id="startButton"><p>Restart</p></button>'
 var roundNames = ["Round 1: No Restrictions", "Round 2: One Word", "Round 3: Only Gestures"];
 
 var game;
+var cardPicker;
 var timer;
 var timeWhenTurnEnded;
 
 $('document').ready(function(){
-
 	var ua = window.navigator.userAgent;
 	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
 	var webkit = !!ua.match(/WebKit/i);
@@ -21,21 +20,26 @@ $('document').ready(function(){
 		$("#container").height("85vh")
 	}
 
-	//If a game is stored in localStorage, it gets restored
-	if(localStorage.getItem("deck") != null) {
-		game = new Game(parseInt(localStorage.getItem("cardNum")), 3, localStorage.getItem("blueName"), localStorage.getItem("redName"));
-		game.restoreState();
-		if(localStorage.getItem("currentPhase") == "newround")
-			roundOver(true);
-		else if(localStorage.getItem("currentPhase") == "newgame")
-			gameAboutToStart();
-		else
-			turnAboutToStart();	
-	}
+    localStorage.clear()
+    getParametersInputForCardPicker()
+
+	// //If a game is stored in localStorage, it gets restored
+	// if(localStorage.getItem("deck") != null) {
+	// 	game = new Game(parseInt(localStorage.getItem("cardNum")), 3, localStorage.getItem("blueName"), localStorage.getItem("redName"));
+	// 	game.restoreState();
+	// 	if(localStorage.getItem("currentPhase") == "newround")
+	// 		roundOver(true);
+	// 	else if(localStorage.getItem("currentPhase") == "newgame")
+	// 		gameAboutToStart();
+	// 	else
+	// 		turnAboutToStart();
+	// } else {
+    //     getParametersInputForCardPicker();
+    // }
 });
 
-function newGame(numCards, team1Name, team2Name) {
-	game = new Game(numCards, 3, team1Name, team2Name);
+function newGame(team1Name, team2Name) {
+	game = new Game(cardPicker.pickedCards(), 3, team1Name, team2Name);
 	game.init();
 }
 
@@ -52,13 +56,13 @@ function correct() {
 	}
 }
 
-//Gets called when the pass button is pressed 
+//Gets called when the pass button is pressed
 function pass() {
 	if(game.pass()) {
 		var next = game.nextCard();
 		if(next == -1)
 			roundOver();
-		else 
+		else
 			document.getElementById("card").src = cardToImg(next);
 	}
 }
@@ -69,44 +73,105 @@ function undo() {
 		document.getElementById("card").src = cardToImg(game.currentCard);
 }
 
+function cardSelectionPhaseButtonClicked() {
+    var numPlayers = $("#numPlayersInput").val();
+    var cardsPerPlayer = $("#cardsPerPlayer").val();
+    if (numPlayers === "" || cardsPerPlayer === "") {
+        alert("Missing at least one input");
+        return
+    }
+
+    startCardSelectionPhase(numPlayers, cardsPerPlayer);
+}
+
+function updateCardPickerScreen() {
+    $("#cardsLeftHeader").text("Cards left: " + cardPicker.getCardsLeft())
+    $("#card").attr('src', cardToImg(cardPicker.getCard()))
+}
+
+function showCardPickerScreen() {
+    $("#cardPicker").show()
+    $("#cardPicker").height("75%");
+    updateCardPickerScreen()
+}
+
+function cardPickerYes() {
+    cardPicker.useCard()
+    handleCardPickPostAction()
+}
+function cardPickerNo() {
+    handleCardPickPostAction()
+}
+
+function handleCardPickPostAction() {
+    if (cardPicker.areAllPlayersDone()) {
+        $("#cardPicker").hide()
+        startTeamSelection()
+        return
+    } else if (cardPicker.isPlayerDone()) {
+        $("#card").css('filter', 'blur(10px)')
+        setTimeout(() => {
+            alert("Next Player!")
+            cardPicker.setupNextPlayer()
+            $("#card").css('filter', '')
+            updateCardPickerScreen()
+        }, 0)
+    } else {
+        updateCardPickerScreen()
+    }
+}
+
+function startCardSelectionPhase(numPlayers, cardsPerPlayer) {
+    cardPicker = new CardPicker(numPlayers, cardsPerPlayer)
+    $("#mainPage").hide()
+    showCardPickerScreen()
+}
+
+function teamSelectionButtonClicked() {
+    var team1 = $("#team1Input").val()
+    var team2 = $("#team2Input").val()
+
+    if (team1 == "" || team2 == "") {
+        alert("At least one input is missing")
+    } else if(team1 == team2) {
+        alert("Team names are identical!")
+    }
+    if(team1 != team2) {
+        $("#teamSelection").hide()
+        newGame(team1, team2)
+        gameAboutToStart();
+        startTurn()
+    }
+}
+
 //When this is called, it is either because the turn is being started or because the settings have been entered and the game is about to start
 function startTurn() {
-	if(game == null) {
-		var numcards = $("#numCardsInput").val();
-		var team1 = $("#team1Input").val();
-		var team2 = $("#team2Input").val();
-		if(numcards > 5 && team1 != "" && team2 != "" && team1 != team2) {
-			newGame(numcards, team1, team2);
-			gameAboutToStart();
-		}
-	}
-	else {
-		//If 2000 milliseconds has not passed since the turn ended, the next turn cannot be started
-		if(new Date().getTime() - timeWhenTurnEnded < 2000)
-			return;
+    //If 2000 milliseconds has not passed since the turn ended, the next turn cannot be started
+    if(new Date().getTime() - timeWhenTurnEnded < 2000)
+        return;
 
-		$("#cardContainer").empty();
-		$("#cardContainer").html('<img id="card" src="' + cardToImg(game.nextCard()) + '"/>');
-		$("#cardContainer").prepend('<div id="timer"></div>');
-		$("#cardContainer").height("75%");
-		$("#buttonContainer").show();
-		var height = 5;
-		timer = $("#timer").countdown360({
-	  		radius      : 28,
-	  		seconds     : 60,
-	  		strokeWidth : 15,
-	  		fillStyle   : '#0276FD',
-	  		strokeStyle : '#003F87',
-	  		fontSize    : 25,
-	  		label: ["", ""],
-	  		fontColor   : '#FFFFFF',
-	  		autostart: false,
-	  		smooth: true,
-	  		onComplete: function(){turnAboutToStart()}
-		})
-		game.startTurn();
-		timer.start();
-	}
+    $("#cardContainer").empty();
+    $("#cardContainer").html('<img id="card" src="' + cardToImg(game.nextCard()) + '"/>');
+    $("#cardContainer").prepend('<div id="timer"></div>');
+    $("#cardContainer").height("75%");
+    $("#cardContainer").show()
+    $("#buttonContainer").show();
+    var height = 5;
+    timer = $("#timer").countdown360({
+        radius      : 28,
+        seconds     : 60,
+        strokeWidth : 15,
+        fillStyle   : '#0276FD',
+        strokeStyle : '#003F87',
+        fontSize    : 25,
+        label: ["", ""],
+        fontColor   : '#FFFFFF',
+        autostart: false,
+        smooth: true,
+        onComplete: function(){turnAboutToStart()}
+    })
+    game.startTurn();
+    timer.start();
 }
 
 function gameAboutToStart() {
@@ -115,11 +180,12 @@ function gameAboutToStart() {
 	timeWhenTurnEnded = new Date().getTime();
 	var html = aboutToStartGameHtml;
 	html = html.replace("%roundname%", roundNames[0]);
-	html = html.replace("%deckCount%", game.cardNum.toString());
+	html = html.replace("%deckCount%", game.deck.length.toString());
 	html = html.replace("%nextteam%", game.getNextTeam());
 	container.html(html);
 	container.append(button);
 	container.height("95%");
+    container.show()
 	game.storeState("newgame");
 }
 
@@ -135,7 +201,6 @@ function turnAboutToStart() {
 	html = html.replace("%deckCount%", game.deck.length.toString());
 	html = html.replace("%nextteam%", game.getNextTeam());
 	html = html.replace("%guessedCount%", game.cardsJustGotten.toString());
-	html = html.replace("%passedCount%", game.cardsJustPassed.toString());
 	container.html(html);
 	container.append(button);
 	container.height("95%");
@@ -186,7 +251,6 @@ function gameOver() {
 	html = html.replace("%team2%", game.redName);
 	html = html.replace("%2teampoints%", game.redPoints.toString());
 	container.html(html);
-	container.append(restartbutton);
 	container.height("95%");
 	game.clearState();
 }
@@ -203,13 +267,12 @@ function pause() {
 	timer.extendTimer(-1 * elapsed)
 }
 
-function restart() {
-	var yes = confirm("Are you sure you want to restart?")
-	if(yes) {
-		game = null;
-		localStorage.clear()
-		$("#buttonContainer").hide()
-		$("#cardContainer").html(defaultHtml)
-		$("#cardContainer").height("95%")
-	}
+function startTeamSelection() {
+    // $("#buttonContainer").hide()
+    $("#teamSelection").show()
 }
+
+function getParametersInputForCardPicker() {
+    $("#mainPage").show()
+}
+
--- js/monikers.js
+++ js/monikers.js
@@ -10,33 +10,68 @@ function shuffleArray(array) {
     return array;
 }
 
-function Game(cardNum, numRounds, team1Name, team2Name) {
-	this.deck
+function CardPicker(numPlayers, cardsPerPlayer) {
+    this.cardsPerPlayer = cardsPerPlayer
+
+    this.playersLeft = numPlayers - 1
+    this.cardsLeftCurrentPlayer = this.cardsPerPlayer
+
+    this.seenCards = []
+    this.usedCards = []
+    this.currentCard
+
+    this.getCard = function() {
+        do {
+            this.currentCard = getRandomCard()
+        } while (this.seenCards.indexOf(this.currentCard) !== -1)
+
+        this.seenCards.push(this.currentCard)
+        return this.currentCard
+    }
+    this.getCardsLeft = function() {
+        return this.cardsLeftCurrentPlayer
+    }
+    this.useCard = function() {
+        this.usedCards.push(this.currentCard)
+        this.cardsLeftCurrentPlayer --
+    }
+    this.isPlayerDone = function() {
+        return this.cardsLeftCurrentPlayer == 0
+    }
+    this.areAllPlayersDone = function() {
+        return this.playersLeft == 0 && this.isPlayerDone()
+    }
+    this.setupNextPlayer = function() {
+        this.cardsLeftCurrentPlayer = this.cardsPerPlayer
+        this.playersLeft --
+    }
+    this.pickedCards  = function() {
+        return this.usedCards
+    }
+}
+
+function getRandomCard() {
+    return Math.ceil(Math.random() * TOTAL_CARD_NUM)
+}
+
+function Game(deck, numRounds, team1Name, team2Name) {
+	this.deck = deck
 	this.bluePile = []
 	this.redPile = []
 	this.blueName = team1Name;
 	this.redName = team2Name;
-	this.passedPile = []
 	this.undoInfo = null
 	this.currentCard = -1
 	this.bluePoints = 0
 	this.redPoints = 0
 	this.currentRound = 1
 	this.numRounds = numRounds
-	this.cardNum = cardNum
 	this.cardsJustGotten = 0
-	this.cardsJustPassed = 0
 	this.cardsInPileAtStartOfTurn
 	//currentTurn = 1 corresponds to blue, -1 corresponds to red
 	this.currentTurn
 	this.init = function() {
-		var deck = []
-		while (deck.length < this.cardNum) {
-			var random = Math.ceil(Math.random() * TOTAL_CARD_NUM)
-			if(deck.indexOf(random) == -1)
-				deck.push(random)	
-		}
-		this.deck = shuffleArray(deck)
+		this.deck = shuffleArray(this.deck)
 
 		if(Math.random() < 0.5)
 			this.currentTurn = -1
@@ -66,13 +101,15 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 			return "It's a tie!"
 	}
 	this.getCardValue = function(card) {
+        // pixel = getPixel(cardToImg(card), 0, 0)
+        // alert(pixel)
 		return 1;
 	}
 	//Whenever a card is passed, it is added to the passed pile
 	this.pass = function() {
 		if(this.currentCard == -1)
 			return false
-		this.passedPile.push(this.currentCard)
+		this.deck.push(this.currentCard)
 		this.undoInfo = [this.currentCard]
 		return true
 	}
@@ -93,21 +130,15 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 	this.nextCard = function() {
 		//If the deck is empty, then the passed pile becomes the deck. If there is no passed pile, the round is over
 		if(this.deck.length == 0) {
-			if(this.passedPile.length == 0) {
-				return -1
-			}
-			else {
-				this.deck = this.passedPile.reverse().slice()
-				this.passedPile = []
-			}
-		}
-		this.currentCard = this.deck.pop()
+            return -1
+        }
+		this.currentCard = this.deck.shift()
 		if(this.undoInfo != null)
 			this.undoInfo.push(this.currentCard)
 		return this.currentCard
 	}
 	//The undo button allows you to undo the last action taken. If you click undo, clicking undo again will do nothing
-	//undoInfo is an array with two values. The first value is the value of the card that was put into the wrong pile. 
+	//undoInfo is an array with two values. The first value is the value of the card that was put into the wrong pile.
 	//The second value is the value of the card that came after the misclicked card
 	//Clicking undo restores the first value as currentCard and puts the second value on top of the deck
 	this.undo = function() {
@@ -120,6 +151,7 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 	}
 	//At the start of the turn, the number of cards in the current teams pile is noted so that the change can be calculated at the end of the turn
 	this.startTurn = function() {
+        this.deck = shuffleArray(this.deck)
 		if(this.currentTurn == 1)
 			this.cardsInPileAtStartOfTurn = this.bluePile.length
 		else
@@ -129,16 +161,12 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 		if(this.currentCard == -1)
 			return false
 		//The number of cards guessed and passed are noted so they can be reported
-		this.cardsJustPassed = this.passedPile.length
 		if(this.currentTurn == 1)
 			this.cardsJustGotten = this.bluePile.length - this.cardsInPileAtStartOfTurn
 		else
 			this.cardsJustGotten = this.redPile.length - this.cardsInPileAtStartOfTurn
 
-		//The last card is put in the passed pile, and then the passed pile is merged with the deck for the next turn
-		this.passedPile.push(this.currentCard)
-		this.deck = this.passedPile.concat(this.deck)
-		this.passedPile = []
+		this.deck.unshift(this.currentCard)
 		this.undoInfo = null
 		this.currentCard = -1
 		this.currentTurn = this.currentTurn * -1;
@@ -148,9 +176,9 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 	//The deck is restored by combining the blue and red piles
 	this.nextRound = function() {
 		this.currentRound += 1
-		for(var i = 0; i < this.redPile.length; i++) 
+		for(var i = 0; i < this.redPile.length; i++)
 			this.redPoints += this.getCardValue(this.redPile[i])
-		for(var i = 0; i < this.bluePile.length; i++) 
+		for(var i = 0; i < this.bluePile.length; i++)
 			this.bluePoints += this.getCardValue(this.bluePile[i])
 
 		this.deck = shuffleArray(this.redPile.concat(this.bluePile))
@@ -173,9 +201,7 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 		localStorage.setItem("redPoints", this.redPoints.toString());
 		localStorage.setItem("currentRound", this.currentRound.toString());
 		localStorage.setItem("currentTurn", this.currentTurn.toString());
-		localStorage.setItem("cardNum", this.cardNum.toString());
 		localStorage.setItem("cardsJustGotten", this.cardsJustGotten.toString());
-		localStorage.setItem("cardsJustPassed", this.cardsJustPassed.toString());
 		localStorage.setItem("currentPhase", currentPhase);
 	}
 	this.restoreState = function() {
@@ -188,9 +214,7 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 		this.redPoints = parseInt(localStorage.getItem("redPoints"));
 		this.currentRound = parseInt(localStorage.getItem("currentRound"));
 		this.currentTurn = parseInt(localStorage.getItem("currentTurn"));
-		this.cardNum = parseInt(localStorage.getItem("cardNum"));
 		this.cardsJustGotten = parseInt(localStorage.getItem("cardsJustGotten"));
-		this.cardsJustPassed = parseInt(localStorage.getItem("cardsJustPassed"));
 	}
 	this.clearState = function() {
 		localStorage.removeItem("deck");
@@ -202,10 +226,17 @@ function Game(cardNum, numRounds, team1Name, team2Name) {
 		localStorage.removeItem("redPoints");
 		localStorage.removeItem("currentRound");
 		localStorage.removeItem("currentTurn");
-		localStorage.removeItem("cardNum");
 		localStorage.removeItem("cardsJustGotten");
-		localStorage.removeItem("cardsJustPassed");
 		localStorage.removeItem("currentPhase");
 	}
-} 
+}
 
+function getPixel(url, x, y) {
+    var img = new Image();
+    img.src = url;
+    img.setAttribute('crossOrigin', '')
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    return context.getImageData(x, y, 1, 1).data;
+}
